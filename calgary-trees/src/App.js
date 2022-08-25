152ae18ddb64f39733a46effae70be60bbad66cc
Aubrey Blair-Pattison <aubrey.blairpattison@gmail.com>
2022-08-24 19:48:04 -0600

    first map draft, no trees

--- calgary-trees/src/App.js
+++ calgary-trees/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {MapContainer, TileLayer, Marker, Popup, useMap} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+const position = [51.047, -114.063]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <MapContainer 
+          center={[51.047, -114.059]} 
+          zoom={13} 
+          scrollWheelZoom={false} 
+          >
+        <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            popup here
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
