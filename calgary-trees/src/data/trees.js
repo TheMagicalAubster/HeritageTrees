7c2572812b2368dbabead28ff06752cfd90dfd4d
Aubrey Blair-Pattison <aubrey.blairpattison@gmail.com>
2022-08-31 16:32:55 -0600

    array movements

--- calgary-trees/src/data/trees.js
+++ calgary-trees/src/data/trees.js
@@ -1,59 +1,25 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { render } from 'react-dom';
 
-class Trees extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: []
-    };
-  }
 
-  componentDidMount() {
-    axios.get("https://data.calgary.ca/resource/tfs4-3wwa.json")
-      .then(response => {
-        const data = response.data;
-        this.setState({ data })
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  }
 
-  render() {
-    return (
-      <div className='Trees'>
-        {this.state.data.map((item, index) => <TreeList key={index} {...item} />)}
-      </div>
-    )
-  }
+
+
+
+async function getTrees() {
+  let trees = await axios.get(
+    "https://data.calgary.ca/resource/tfs4-3wwa.json"
+  );
+  // console.log(trees.data[0].asset_type);
+  // console.log(trees.data[0].asset_subtype);
+  // console.log(trees.data[0].common_name);
+  // console.log(trees.data[0].mature_size);
+  // console.log(trees.data[0].maintained_by);
+  // console.log(trees.data[0].tree_maintenance);
+  // console.log(trees.data[0].point.coordinates);
+  trees.data.map((item, index) => console.log(item.point.coordinates, index));
 }
 
-const TreeList = (props) => (
-  <p> 
-    HI
-    {props}
-  </p>
-)
-
-export default Trees;
-
-
-
-
-
-// async function getTrees() {
-//   let trees = await axios.get(
-//     "https://data.calgary.ca/resource/tfs4-3wwa.json"
-//   );
-//   // console.log(trees.data[0].asset_type);
-//   // console.log(trees.data[0].asset_subtype);
-//   // console.log(trees.data[0].common_name);
-//   // console.log(trees.data[0].mature_size);
-//   // console.log(trees.data[0].maintained_by);
-//   // console.log(trees.data[0].tree_maintenance);
-//   console.log(trees.data[0].point.coordinates);
-// }
-
-// getTrees();
+getTrees();
